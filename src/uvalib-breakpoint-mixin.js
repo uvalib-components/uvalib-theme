fcb527fb76259354d6a0232c039cc32fc53b8d6c
Doug Chestnut <dougchestnut@gmail.com>
2019-10-09 14:42:04 -0400

    move breakpoints to mixin as we should only do this if really needed

---
+++ src/uvalib-breakpoint-mixin.js
@@ -0,0 +1,34 @@
+import {property} from 'lit-element';
+export let uvalibBreakpointMixin = (superclass) => class extends superclass {
+  @property({type: Boolean, reflect: true, attribute: 'small-screen'}) smallScreen = false;
+  @property({type: Boolean, reflect: true, attribute: 'medium-screen'}) mediumScreen = false;
+  @property({type: Boolean, reflect: true, attribute: 'large-screen'}) largeScreen = false;
+  firstUpdated() {
+    const small = window.matchMedia("(max-width: 600px)");
+    if (small.matches) this.__setscreen('small');
+    small.addListener(q=>{ if (q.matches) this.__setscreen('small'); });
+    const medium = window.matchMedia("(min-width: 600px) and (max-width: 992px)");
+    if (medium.matches) this.__setscreen('medium');
+    medium.addListener(q=>{ if (q.matches) this.__setscreen('medium'); });
+    const large = window.matchMedia("(min-width: 993px)");
+    if (large.matches) this.__setscreen('large');
+    large.addListener(q=>{ if (q.matches) this.__setscreen('large'); });
+  }
+  __setscreen(sz){
+    if (sz == "small") {
+      this.smallScreen = true;
+      this.mediumScreen = false;
+      this.largeScreen = false;
+    }
+    else if (sz == "medium") {
+      this.smallScreen = false;
+      this.mediumScreen = true;
+      this.largeScreen = false;
+    }
+    else {
+      this.smallScreen = false;
+      this.mediumScreen = false;
+      this.largeScreen = true;
+    }
+  }
+};
--- src/uvalib-lit-element.js
+++ src/uvalib-lit-element.js
@@ -1,9 +1,7 @@
 import {html, LitElement, property} from 'lit-element';
+import { uvalibBreakpointMixin } from './uvalib-breakpoint-mixin';
 
-export class UvaLitElement extends LitElement {
-  @property({type: Boolean, reflect: true, attribute: 'small-screen'}) smallScreen = false;
-  @property({type: Boolean, reflect: true, attribute: 'medium-screen'}) mediumScreen = false;
-  @property({type: Boolean, reflect: true, attribute: 'large-screen'}) largeScreen = false;
+export class UvaLitElement extends uvalibBreakpointMixin(LitElement) {
   render() {
     return html`
       <p>Hello World</p>
@@ -12,36 +10,7 @@ export class UvaLitElement extends LitElement {
       ${this.largeScreen? html`<h1>Large Screen</h1>`:''}
     `;
   }
-  constructor() {
-    super();
-
-    const small = window.matchMedia("(max-width: 600px)");
-    if (small.matches) this.__setscreen('small');
-    small.addListener(q=>{ if (q.matches) this.__setscreen('small'); });
-    const medium = window.matchMedia("(min-width: 600px) and (max-width: 992px)");
-    if (medium.matches) this.__setscreen('medium');
-    medium.addListener(q=>{ if (q.matches) this.__setscreen('medium'); });
-    const large = window.matchMedia("(min-width: 993px)");
-    if (large.matches) this.__setscreen('large');
-    large.addListener(q=>{ if (q.matches) this.__setscreen('large'); });
-  }
-  __setscreen(sz){
-    if (sz == "small") {
-      this.smallScreen = true;
-      this.mediumScreen = false;
-      this.largeScreen = false;
-    }
-    else if (sz == "medium") {
-      this.smallScreen = false;
-      this.mediumScreen = true;
-      this.largeScreen = false;
-    }
-    else {
-      this.smallScreen = false;
-      this.mediumScreen = false;
-      this.largeScreen = true;
-    }
-  }
+  
 }
 
 customElements.define('uvalib-lit-element', UvaLitElement);
